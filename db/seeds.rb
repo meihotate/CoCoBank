16663e47215240a73d76898520ddb9223db83040
meihotate <meihotate@gmail.com>
2019-09-25 06:33:53 +0000

    delete contents of seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create(name: 'mei', email: 'm.h0tate@gmail.com', password: 'password')
-
 User.create!(
   [
     {
