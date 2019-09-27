e9cdfe2852d27f10e9570b472159a46af62f4718
meihotate <meihotate@gmail.com>
2019-09-27 07:45:44 +0000

    Add routing access varidation

--- app/controllers/users/chatrooms_controller.rb
+++ app/controllers/users/chatrooms_controller.rb
@@ -1,16 +1,27 @@
 class Users::ChatroomsController < ApplicationController
   def show
-    nowdate = DateTime.now
-  	chatroom = Chatroom.find(params[:id])
-    chatroom.update(accesstime: nowdate)
-  	@member = Chatmember.find_by(user_id: current_user.id, chatroom_id: chatroom.id)
-  	@other_user = User.find(params[:other_user_id])
-  	@message = Chatmessage.new
+    chatroom = Chatroom.find(params[:id])
+    if current_user.id.to_s != params[:other_user_id]
+      # binding.pry
+        if Chatmember.with_deleted.where(chatroom_id: params[:id], user_id: current_user.id) != ([] || nil) && Chatmember.with_deleted.where(chatroom_id: params[:id], user_id: params[:other_user_id]) != ([] || nil)
+            # binding.pry
+            nowdate = DateTime.now
+            chatroom.update(accesstime: nowdate)
+          	@member = Chatmember.with_deleted.find_by(user_id: current_user.id, chatroom_id: chatroom.id)
+          	@other_user = User.with_deleted.find(params[:other_user_id])
+          	@message = Chatmessage.new
 
-  	if Chatmessage.where(chatroom_id: chatroom.id)
-  		@chatmessages = Chatmessage.where(chatroom_id: chatroom.id).order(created_at: :asc)
-  	end
-
+          	if Chatmessage.with_deleted.where(chatroom_id: chatroom.id)
+          		@chatmessages = Chatmessage.with_deleted.where(chatroom_id: chatroom.id).order(created_at: :asc)
+          	end
+        else
+            flash[:notice] = "不正なアクセスです"
+            redirect_to users_show_path(current_user)
+        end
+    else
+      flash[:notice] = "不正なアクセスです"
+      redirect_to users_show_path(current_user)
+    end
   end
 
   def create
